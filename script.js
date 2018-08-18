e73a51eac7c87967b65558927dd26f7b9d394bb1
Liam Cavens <liamcavens@googlemail.com>
2018-08-18 13:39:33 +0100

    added scroll to with links

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-window.addEventListener('scroll', fixHeader);
+window.addEventListener("scroll", fixHeader);
 
-const navbar = document.getElementById('nav-bar');
+const navbar = document.getElementById("nav-bar");
 const sticky = navbar.offsetTop;
 
 function fixHeader() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("navbar-sticky");
-    } else {
-        navbar.classList.remove("navbar-sticky");
-    }
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("navbar-sticky");
+  } else {
+    navbar.classList.remove("navbar-sticky");
+  }
 }
 
 function toggleCodes(on) {
-    var obj = document.getElementById('icons');
+  const obj = document.getElementById("icons");
 
-    if (on) {
-        obj.className += ' codesOn';
-    } else {
-        obj.className = obj.className.replace(' codesOn', '');
-    }
+  if (on) {
+    obj.className += " codesOn";
+  } else {
+    obj.className = obj.className.replace(" codesOn", "");
+  }
+}
+
+
+const offset = 45;
+const bodyRect = document.body.getBoundingClientRect().top;
+const elementRect = element.getBoundingClientRect().top;
+const elementPosition = elementRect - bodyRect;
+const offsetPosition = elementPosition - offset;
+
+function jumpTo(element) {
+    const ele = document.getElementById(element);
+    const offset = 60;
+    const bodyRect = document.body.getBoundingClientRect().top;
+    const elementRect = ele.getBoundingClientRect().top;
+    const elementPosition = elementRect - bodyRect;
+    const offsetPosition = elementPosition - offset;
+
+    window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+    });
 }
